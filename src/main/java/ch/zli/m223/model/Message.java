a3dd43772b808d48ab15b1afa328b698904efb5f
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-04 19:41:58 +0000

    Chore: Add Entities and Relations

--- src/main/java/ch/zli/m223/model/Message.java
+++ src/main/java/ch/zli/m223/model/Message.java
@@ -1,5 +1,68 @@
 package ch.zli.m223.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "messages")
 public class Message {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "sender_id")
+    private Member sender;
+
+    @ManyToOne
+    @JoinColumn(name = "receiver_id")
+    private Member receiver;
+
+    private String content;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date timestamp;
+
+    // Konstruktor, Getter und Setter
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getSender() {
+        return sender;
+    }
+
+    public void setSender(Member sender) {
+        this.sender = sender;
+    }
+
+    public Member getReceiver() {
+        return receiver;
+    }
+
+    public void setReceiver(Member receiver) {
+        this.receiver = receiver;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(Date timestamp) {
+        this.timestamp = timestamp;
+    }
 }

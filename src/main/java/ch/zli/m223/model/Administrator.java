50253ca0b9b9bcf8965c2da5a661ec4b6a795db4
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-04 19:10:48 +0000

    Chore: Add Entities and Relations

--- src/main/java/ch/zli/m223/model/Administrator.java
+++ src/main/java/ch/zli/m223/model/Administrator.java
@@ -1,5 +1,56 @@
 package ch.zli.m223.model;
 
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "administrators")
 public class Administrator {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToMany(mappedBy = "administrator")
+    private List<Booking> bookings;
+
+    @OneToMany(mappedBy = "receiver")
+    private List<Message> receivedMessages;
+
+    @OneToMany(mappedBy = "sender")
+    private List<Message> sentMessages;
+
     
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public List<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public List<Message> getReceivedMessages() {
+        return receivedMessages;
+    }
+
+    public void setReceivedMessages(List<Message> receivedMessages) {
+        this.receivedMessages = receivedMessages;
+    }
+
+    public List<Message> getSentMessages() {
+        return sentMessages;
+    }
+
+    public void setSentMessages(List<Message> sentMessages) {
+        this.sentMessages = sentMessages;
+    }
+
 }

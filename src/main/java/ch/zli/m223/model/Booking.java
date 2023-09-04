3fd3152e25851e988860e1569dc3d7b49b39844d
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-04 19:14:21 +0000

    Chore: Add Entities and Relations

--- src/main/java/ch/zli/m223/model/Booking.java
+++ src/main/java/ch/zli/m223/model/Booking.java
@@ -1,5 +1,80 @@
 package ch.zli.m223.model;
 
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "bookings")
 public class Booking {
-    
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne
+    @JoinColumn(name = "resource_id")
+    private Resource resource;
+
+    @ManyToOne
+    @JoinColumn(name = "administrator_id")
+    private Administrator administrator;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date date;
+
+    @Enumerated(EnumType.STRING)
+    private BookingStatus status;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Resource getResource() {
+        return resource;
+    }
+
+    public void setResource(Resource resource) {
+        this.resource = resource;
+    }
+
+    public Administrator getAdministrator() {
+        return administrator;
+    }
+
+    public void setAdministrator(Administrator administrator) {
+        this.administrator = administrator;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public BookingStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(BookingStatus status) {
+        this.status = status;
+    }
 }

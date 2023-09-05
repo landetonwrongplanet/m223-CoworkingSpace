5aafef159ce4d68814defac6729962a0a5af2095
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-05 12:40:49 +0000

    Chore: Add new Endpoint Administrator

--- src/main/java/ch/zli/m223/controller/AdministratorController.java
+++ src/main/java/ch/zli/m223/controller/AdministratorController.java
@@ -1,5 +1,61 @@
 package ch.zli.m223.controller;
 
+import javax.inject.Inject;
+import javax.ws.rs.Path;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import java.util.List;
+import javax.annotation.security.RolesAllowed;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.eclipse.microprofile.openapi.annotations.Operation;
+
+import ch.zli.m223.model.Administrator;
+
+import ch.zli.m223.service.AdministratorService;
+
+
+@Path("/administrator")
+@Tag(name = "Administrators", description = "Administratorss and their handling")
+@RolesAllowed({  "Admin" })
 public class AdministratorController {
     
+    @Inject
+    AdministratorService administratorService;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "Index all administrators.", 
+        description = "Returns a list of all administrators."
+    )
+    public List<Administrator> index() {
+        return administratorService.findAll();
+    }
+
+    @POST
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Operation(
+        summary = "Creates a new Admin.", 
+        description = "Creates a new Admin and returns the newly added admin."
+    )
+    public Administrator create(Administrator administrator) {
+       return administratorService.createAdministrator(administrator);
+    }
+
+
+
+
+
 }
+
+
+
+
--- src/main/java/ch/zli/m223/service/AdministratorService.java
+++ src/main/java/ch/zli/m223/service/AdministratorService.java
@@ -1,5 +1,41 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
+import ch.zli.m223.model.Administrator;
+import ch.zli.m223.model.Booking;
+
+@ApplicationScoped
 public class AdministratorService {
+
+    public List<Administrator> getAllAdministrators() {
+        return null;
+    }
     
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public Administrator createAdministrator(Administrator administrator) {
+        return entityManager.merge(administrator);
+    }
+
+
+
+ public List<Administrator> findAll() {
+     var query = entityManager.createQuery("FROM Administrator", Administrator.class);
+    return query.getResultList();
+    }
+    
+
+    public Booking getAdministratorById(Long id) {
+        return null;
+    }
+
+
 }

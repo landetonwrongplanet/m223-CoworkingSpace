42d7fff6801f23aba4cec4c8199025d996254479
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-05 07:09:41 +0000

    Chore: Adapt  Registration Request

--- src/main/java/ch/zli/m223/controller/MemberRegistrationRequest.java
+++ src/main/java/ch/zli/m223/controller/MemberRegistrationRequest.java
@@ -2,16 +2,32 @@ package ch.zli.m223.controller;
 
 public class MemberRegistrationRequest {
 
-    public Object getName() {
-        return null;
+    private String name;
+    private String email;
+    private String password;
+
+    public String getName() {
+        return name;
     }
 
-    public Object getEmail() {
-        return null;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public Object getPassword() {
-        return null;
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 
 }

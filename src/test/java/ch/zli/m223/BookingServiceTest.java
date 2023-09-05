45a79efc6616201ecb81cd790302b9fccbd32bd8
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-05 12:19:22 +0000

    Chore: Add BookingServiceTest

--- src/test/java/ch/zli/m223/BookingServiceTest.java
+++ src/test/java/ch/zli/m223/BookingServiceTest.java
@@ -1,5 +1,34 @@
 package ch.zli.m223;
 
+import ch.zli.m223.service.BookingService;
+
+import ch.zli.m223.model.Booking;
+
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+import javax.inject.Inject;
+
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@QuarkusTest
 public class BookingServiceTest {
+
+    @Inject
+    BookingService bookingService;
+
+
+
+  
+
+    @Test
+    public void testGetAllBookings() {
     
+        List<Booking> bookings = bookingService.getAllBookings();
+
+        assertNotNull(bookings);
+    }
 }

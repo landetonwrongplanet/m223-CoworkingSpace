89b4f6ebf9aed4dbcb8460a3c8fa95514d3a53fc
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-05 07:21:31 +0000

    Chore: Handling Booking as Admin

---
+++ src/main/java/ch/zli/m223/controller/BookingRequest.java
@@ -0,0 +1,17 @@
+package ch.zli.m223.controller;
+
+public class BookingRequest {
+
+    public Object getMemberId() {
+        return null;
+    }
+
+    public Object getResourceId() {
+        return null;
+    }
+
+    public Object getDate() {
+        return null;
+    }
+
+}
--- src/main/java/ch/zli/m223/service/BookingService.java
+++ src/main/java/ch/zli/m223/service/BookingService.java
@@ -1,5 +1,28 @@
 package ch.zli.m223.service;
 
+import java.util.List;
+
+import ch.zli.m223.model.Booking;
+
 public class BookingService {
+
+    public List<Booking> getAllBookings() {
+        return null;
+    }
+
+    public Booking getBookingById(Long id) {
+        return null;
+    }
+
+    public void deleteBooking(Long id) {
+    }
+
+    public Booking createBooking(Object memberId, Object resourceId, Object date) {
+        return null;
+    }
+
+    public Booking updateBooking(Long id, Object memberId, Object resourceId, Object date) {
+        return null;
+    }
     
 }

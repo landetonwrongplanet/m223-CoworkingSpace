86e2b1dea47f04c07541eb4aa4ff4a3f6e9fb435
landetonwrongplanet <mail@ileniacastano.ch>
2023-09-05 09:12:37 +0000

    Chore: Add Service Class

--- src/main/java/ch/zli/m223/service/BookingService.java
+++ src/main/java/ch/zli/m223/service/BookingService.java
@@ -2,6 +2,11 @@ package ch.zli.m223.service;
 
 import java.util.List;
 
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
+
 import ch.zli.m223.model.Booking;
 
 public class BookingService {
@@ -9,32 +14,36 @@ public class BookingService {
     public List<Booking> getAllBookings() {
         return null;
     }
+    @ApplicationScoped
+    @Inject
+    EntityManager entityManager;
+
+    @Transactional
+    public Booking createBooking(Booking booking) {
+        return entityManager.merge(booking);
+    }
+
+    @Transactional
+    public void deleteBooking(Long id) {
+        var entity = entityManager.find(Booking.class, id);
+        entityManager.remove(entity);
+    }
+
+    @Transactional
+    public Booking updateBooking(Long id, Booking booking) {
+        booking.setId(id);
+        return entityManager.merge(booking);
+    }
+
+ public List<Booking> findAll() {
+     var query = entityManager.createQuery("FROM Category", Booking.class);
+    return query.getResultList();
+    }
+    
 
     public Booking getBookingById(Long id) {
         return null;
     }
 
-    public void deleteBooking(Long id) {
-    }
 
-    public Booking createBooking(Object memberId, Object resourceId, Object date) {
-        return null;
-    }
-
-    public Booking updateBooking(Long id, Object memberId, Object resourceId, Object date) {
-        return null;
-    }
-
-    public Booking createBooking(Booking booking) {
-        return null;
-    }
-
-    public Booking updateBooking(Long id, Booking booking) {
-        return null;
-    }
-
-    public List<Booking> findAll() {
-        return null;
-    }
-    
 }
